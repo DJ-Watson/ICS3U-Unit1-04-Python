dc834428840fb83f5f47aeabcee91346267be7f2
Ubuntu <ubuntu@ip-172-31-28-83.ec2.internal>
2019-09-13 17:11:12 +0000

    added unit 1-04 file

---
+++ perimeter_area.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+# Created by DJ Watson
+# Created on September 2019
+# This is a program for finidng the area of a rectangle
+
+
+def main():
+    print("If a rectangle has these dimensions:")
+    print("5 mm x 3 mm")
+    print("PERIMETER: (5+3)/2= {}mm" .format((5+3)/2))
+    print("AREA: 5*3= {}mm" .format(5*3))
+
+
+if __name__ == "__main__":
+    main()
